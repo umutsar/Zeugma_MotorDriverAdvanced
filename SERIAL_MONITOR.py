59009b8bbc80e4e17268d2cffe674a2176a17150
superuser <superuser@kali.superuser>
2024-12-11 02:00:50 +0300

    Serial Monitor updated

--- SERIAL_MONITOR.py
+++ SERIAL_MONITOR.py
@@ -2,25 +2,20 @@ import serial
 import sys
 import os
 
-# Seri port ve baudrate ayarları
 port = '/dev/ttyACM0'
 baudrate = 250000
 
-# Sarı renkte yazdırmak için ANSI kaçış kodları
 YELLOW = "\033[33m"
 RESET = "\033[0m"
 
-# İlk değerleri tutmak için bir sözlük
 initial_values = {}
 
-# Ekranı temizlemek için fonksiyon
 def clear_screen():
     if os.name == 'nt':
         os.system('cls')
     else:
         print("\033[2J\033[H", end='')
 
-# Seri portu açma işlemi
 try:
     ser = serial.Serial(port, baudrate, timeout=1)
     print(f"Serial port {port} opened successfully.")
@@ -30,28 +25,25 @@ except serial.SerialException as e:
 
 try:
     while True:
-        # Yeni verinin gelip gelmediğini kontrol et
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             
             clear_screen()
             
-            # Anahtar-değer çiftlerini ayır
             dizi = line.split(",")
             for item in dizi:
                 key, value = item.split(":")
-                value = value.strip()  # Gereksiz boşlukları sil
+                value = value.strip() 
 
-                # Değer değişmiş mi kontrol et
                 if key in initial_values:
                     if initial_values[key] != value:
-                        print(f"{key}: {YELLOW}{value}{RESET}")  # Değişmişse sarı renkte yaz
-                        initial_values[key] = value  # Yeni değeri kaydet
+                        print(f"{key}: {YELLOW}{value}{RESET}")
+                        initial_values[key] = value 
                     else:
-                        print(f"{key}: {value}")  # Değişmemişse normal yaz
+                        print(f"{key}: {value}")
                 else:
-                    initial_values[key] = value  # İlk kez görüyorsak kaydet
-                    print(f"{key}: {value}")  # İlk değerler normal yazılır
+                    initial_values[key] = value 
+                    print(f"{key}: {value}")
 
 except KeyboardInterrupt:
     print("\nExiting...")
